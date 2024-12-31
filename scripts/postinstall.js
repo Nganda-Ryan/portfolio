a7d4aa1506a31dc199e97efef560866abd456267
Rai <rai.ebogo@laakam.it>
2024-12-31 01:44:01 +0100

    remove react-animated-cursor and improve the script to fix some dependency installations error

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 
 try {
-  execSync('npm install --force', { stdio: 'inherit' });
+    execSync('npm install --legacy-peer-deps', { stdio: 'inherit' });
 } catch (error) {
   console.error('Error during postinstall:', error);
 }
