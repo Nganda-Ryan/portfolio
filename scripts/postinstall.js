c54804a195988e776785743192a1dced2ee51639
Rai <rai.ebogo@laakam.it>
2024-12-31 01:58:58 +0100

    remove scripts folder to fix some dependencies installation error

--- scripts/postinstall.js
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'child_process';
-
-try {
-    execSync('npm install --legacy-peer-deps', { stdio: 'inherit' });
-} catch (error) {
-  console.error('Error during postinstall:', error);
-}
